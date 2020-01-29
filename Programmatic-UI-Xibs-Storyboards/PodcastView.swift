95105161d9aa5818febddae708b3ef0924d5095b
Matthew Ramos <mmramos309@gmail.com>
2020-01-29 14:08:52 -0500

    Initial Commit

---
+++ Programmatic-UI-Xibs-Storyboards/PodcastView.swift
@@ -0,0 +1,26 @@
+//
+//  PodcastView.swift
+//  Programmatic-UI-Xibs-Storyboards
+//
+//  Created by Alex Paul on 1/29/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class PodcastView: UIView {
+
+  override init(frame: CGRect) {
+    super.init(frame: UIScreen.main.bounds)
+    commonInit()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    commonInit()
+  }
+  
+  private func commonInit() {
+    // code here
+  }
+}
---
+++ Programmatic-UI-Xibs-Storyboards/PodcastViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Programmatic-UI-Xibs-Storyboards
+//
+//  Created by Alex Paul on 1/29/20.
+//  Copyright © 2020 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class PodcastViewController: UIViewController {
+  
+  private let podcastView = PodcastView()
+  
+  private var podcasts = [Podcast]() {
+    didSet {
+      // code here
+    }
+  }
+
+  override func loadView() {
+    view = podcastView
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+    navigationItem.title = "Podcasts"
+    fetchPodcasts()
+  }
+  
+  private func fetchPodcasts(_ name: String = "swift") {
+    PodcastAPIClient.fetchPodcast(with: name) { (result) in
+      switch result {
+      case .failure(let appError):
+        print("error fetching podcasts: \(appError)")
+      case .success(let podcasts):
+        self.podcasts = podcasts
+      }
+    }
+  }
+}
+
+extension PodcastViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 50
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "podcastCell", for: indexPath)
+    return cell
+  }
+}
+
+
+
+
